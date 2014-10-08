6fdbe9943b3c7894ea3343ac0996eb8a971875d0
roliveros-ramos <ricardo.oliveros@gmail.com>
2014-10-08 08:03:56 -1000

    Help with roxygen2

--- R/calibraR-main.R
+++ R/calibraR-main.R
@@ -1,23 +1,64 @@
-# TO_DO: replicates for fn
-# check par: length(par)==1 provide the number of parameters, guess=rep(NA, par)?
-# add npar? Check compatibility or use it to set the calibration, guess=rep(NA, npar)
-# Restart
-# Should function be taken from restart or arguments?
-# Should we take control from new control or restart?
-# Now: to reproduce actual functioning, think on mutating objects.
+# calibrar package: Calibration of Ecological models using Evoluti --------
 
-# save restart in run dir if not null.
-# use default restart.file if not defined, build over model? parse function name?
-# this has to be inherited from calibrar() to include phase and model
-# define saveRestart?
-# messages by REPORT
+#' Calibration of Ecological Models using Evolutionary Algorithms
+#' 
+#' Calibration of Ecological Models using Evolutionary Algorithms
+#' 
+#' \tabular{ll}{ Package: \tab calibraR\cr Type: \tab Package\cr Version: \tab
+#' 0.1\cr Date: \tab 2014-09-14\cr License: \tab GPL-2\cr } calibrate()
+#' optimES()
+#' 
+#' @name calibraR-package
+#' @aliases calibraR-package calibraR
+#' @docType package
+#' @author Ricardo Oliveros-Ramos Maintainer: Ricardo Oliveros-Ramos
+#' <ricardo.oliveros@@gmail.com>
+#' @references calibrar: an R package for the calibration of ecological models
+#' @keywords calibration
+#' @examples
+#' 
+#' calibrate(par=rep(NA, 5), fn=SphereN)
+#' calibrate(par=rep(NA, 5), fn=SphereN, replicates=3)
+#' calibrate(par=rep(0.5, 5), fn=SphereN, replicates=3, lower=-5, upper=5)
+#' calibrate(par=rep(0.5, 5), fn=SphereN, replicates=3, lower=-5, upper=5, phases=c(1,1,1,2,3))
+#' calibrate(par=rep(0.5, 5), fn=SphereN, replicates=c(1,1,4), lower=-5, upper=5, phases=c(1,1,1,2,3))
+#' 
+NULL
 
-# model-specific packages can be designed over calibrar.
+# optimES -----------------------------------------------------------------
 
-
-optimES = function (par, fn, gr = NULL, ..., method = "default", 
-                    lower = -Inf, upper = Inf, active=NULL, 
-                    control = list(), hessian = FALSE, restart=NULL) {
+#' @title Optimization using evolutionary strategies
+#' @description This function performs the optimization of a function using 
+#' evolutionary strategies, by default the AHR-ES (Oliveros & Shin, 2014). 
+#' @param par A numeric vector. The length of the par argument defines the 
+#' number of parameters to be estimated (i.e. the dimension of the problem).
+#' @param fn The function to be minimized.
+#' @param gr the gradient of \code{fn}. Ignored, added for portability with
+#' other optimization functions.
+#' @param \dots Additional parametrs to be passed to \code{fn}.
+#' @param lower Lower threshold value(s) for parameters. One value or a vector 
+#' of the same length as par. If one value is provided, it is used for all 
+#' parameters. \code{NA} means \code{-Inf}. By default \code{-Inf} is used (unconstrained).
+#' @param upper Upper threshold value(s) for parameters. One value or a vector 
+#' of the same length as par. If one value is provided, it is used for all 
+#' parameters. \code{NA} means \code{Inf}. By default \code{Inf} is used (unconstrained). 
+#' @param active A boolean vector of the same length of \code{par}. If \code{TRUE}, the
+#' parameter is optimized, if \code{FALSE} the parameter is fixed to the value specified
+#' in \code{par}.
+#' @param control Parameter for the control of the algorithm itself, see details.
+#' @param hessian Logical. Should a numerically differentiated Hessian matrix be returned?
+#' Currently not implemented. 
+#' @param method The optimization method to be used. Currently, the only implemented
+#' is the 'default' method, corresponding to the AHR-ES (Oliveros & Shin, 2014).
+#' @param restart If restart is an object of class \code{optimES.restart}, it continue the
+#' optimization using the information from that object.
+#' @author Ricardo Oliveros-Ramos
+#' @examples
+#' calibrate(par=rep(1, 5), fn=SphereN)
+#' @export
+optimES = function (par, fn, gr = NULL, ..., lower = -Inf, upper = Inf, active=NULL, 
+                    control = list(), hessian = FALSE, method = "default", 
+                    restart=NULL) {
   
   npar = length(par)
 
@@ -145,11 +186,47 @@ optimES = function (par, fn, gr = NULL, ..., method = "default",
   
 }
 
+# calibrate ---------------------------------------------------------------
 
-calibrate = function(par, fn, ..., aggFn = NULL, method = "default",
-                     phases = NULL, replicates=1, lower = -Inf, upper = Inf,  
-                     gr = NULL, control = list(), hessian = FALSE, 
-                     restart = NULL) {
+#' @title Sequential calibration of models
+#' @description This function performs the optimization of a function, possibly 
+#' in sequential phases of increasing complexity, and it is designed for the 
+#' calibration of a model, by minimizing the error function \code{fn} associated to it.  
+#' @param par A numeric vector. The length of the par argument defines the 
+#' number of parameters to be estimated (i.e. the dimension of the problem).
+#' @param fn The function to be minimized.
+#' @param \dots Additional parametrs to be passed to \code{fn}.
+#' @param aggFn A function to aggregate \code{fn} to a scalar value if the
+#' returned value is a vector. Some optimization algorithm can explote the
+#' additional information provided by a vectorial output from \code{fn}
+#' @param phases An optional vector of the same length as \code{par}, 
+#' indicating the phase at which each parameter becomes active. If omitted, 
+#' default value is 1 for all parameters, performing a single optimization.
+#' @param replicates The number of replicates for the evaluation of \code{fn}.
+#' The default value is 1. A value greater than 1 is only useful for stochastic
+#' functions.
+#' @param lower Lower threshold value(s) for parameters. One value or a vector 
+#' of the same length as par. If one value is provided, it is used for all 
+#' parameters. \code{NA} means \code{-Inf}. By default \code{-Inf} is used (unconstrained).
+#' @param upper Upper threshold value(s) for parameters. One value or a vector 
+#' of the same length as par. If one value is provided, it is used for all 
+#' parameters. \code{NA} means \code{Inf}. By default \code{Inf} is used (unconstrained). 
+#' @param gr the gradient of \code{fn}. Ignored, added for portability with
+#' other optimization functions.
+#' @param control Parameter for the control of the algorithm itself, see details.
+#' @param hessian Logical. Should a numerically differentiated Hessian matrix be returned?
+#' Currently not implemented. 
+#' @param method The optimization method to be used. Currently, the only implemented
+#' is the 'default' method, corresponding to the AHR-ES (Oliveros & Shin, 2014).
+#' @param restart If restart is an object of class \code{calibrar.restart}, it continue the
+#' calibration using the information from that object.
+#' @author Ricardo Oliveros-Ramos
+#' @examples
+#' calibrate(par=rep(1, 5), fn=SphereN)
+#' @export
+calibrate = function(par, fn, ..., aggFn = NULL, phases = NULL, replicates=1, 
+                     lower = -Inf, upper = Inf,  gr = NULL, control = list(), 
+                     hessian = FALSE, method = "default", restart = NULL) {
   
   npar = length(par)
   
@@ -208,10 +285,24 @@ calibrate = function(par, fn, ..., aggFn = NULL, method = "default",
   
 }
 
+# getObservedData ---------------------------------------------------------
 
-# getObservedData
-
-
+#' @title Get observed data for a model calibration 
+#' 
+#' @description Create a list with the observed data with the 
+#' information provided by its main argument. 
+#' 
+#' @param info A data.frame with the information about the calibration, 
+#' normally created with the \code{\link{getCalibrationInfo}} function. 
+#' See details.
+#' @param path Path to the directory to look up for the data.
+#' @param data.folder folder in the path containing the data.
+#' @param \dots Additional arguments to \code{read.csv} function 
+#' to read the data files.
+#' @return A list with the observed data needed for a calibration, to be used 
+#' in combination with the \code{\link{createObjectiveFunction}}.
+#' @author Ricardo Oliveros-Ramos
+#' @seealso \code{\link{createObjectiveFunction}}, \code{\link{getCalibrationInfo}}.
 getObservedData = function(info, path, data.folder="data", ...) {
   
   observed  = list()
@@ -236,8 +327,21 @@ getObservedData = function(info, path, data.folder="data", ...) {
   
 }
 
-# get Calibration Info
-
+#' Get information for a calibration using the \code{calibrar} package.
+#' 
+#' A wrapper for \code{read.csv} checking column names and data types 
+#' for the table with the calibration information.
+#' 
+#' @param path The path to look for the file.
+#' @param file The file with the calibration information, see details.
+#' @param stringsAsFactors To be passed to \code{read.csv}.
+#' @param \dots Additional arguments to \code{read.csv} function.
+#' @return A data.frame with the information for the calibration of a 
+#' model, to be used with the \code{\link{createObjectiveFunction}} 
+#' and \code{\link{getObservedData}}.
+#' @author Ricardo Oliveros-Ramos
+#' @seealso \code{\link{createObjectiveFunction}}, \code{\link{getObservedData}}.
+#' 
 getCalibrationInfo = function(path, file="calibrationInfo.csv", 
                               stringsAsFactors=FALSE, ...) {
   
@@ -271,8 +375,23 @@ getCalibrationInfo = function(path, file="calibrationInfo.csv",
   return(calibrationInfo)
 }
 
-# createObjectiveFunction
-
+#' Create and objective function to be used with optimization routines
+#' 
+#' Create a new function, to be used as the objective function in the 
+#' calibration, given a function to run the model within R, observed data 
+#' and information about the comparison with data.
+#' 
+#' @param runModel %% ~~Describe \code{runModel} here~~
+#' @param info %% ~~Describe \code{info} here~~
+#' @param observed %% ~~Describe \code{observed} here~~
+#' @param aggFn %% ~~Describe \code{aggFn} here~~
+#' @param aggregate %% ~~Describe \code{aggregate} here~~
+#' @param \dots %% ~~Describe \code{\dots} here~~
+#' @return A function, integrating the simulation of the model and the 
+#' comparison with observed data. 
+#' @author Ricardo Oliveros-Ramos
+#' @seealso \code{\link{getObservedData}}, \code{\link{getCalibrationInfo}}.
+#' 
 createObjectiveFunction = function(runModel, info, observed, aggFn=.weighted.sum, 
                                    aggregate=FALSE, ...) {
 
@@ -317,6 +436,45 @@ createObjectiveFunction = function(runModel, info, observed, aggFn=.weighted.sum
 
 # calibrar Demo
 
+
+
+#' %% ~~function to do ... ~~
+#' 
+#' %% ~~ A concise (1-5 lines) description of what the function does. ~~
+#' 
+#' %% ~~ If necessary, more details than the description above ~~
+#' 
+#' @param path %% ~~Describe \code{path} here~~
+#' @param model %% ~~Describe \code{model} here~~
+#' @param \dots %% ~~Describe \code{\dots} here~~
+#' @return %% ~Describe the value returned %% If it is a LIST, use %%
+#' \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
+#' 'comp2'} %% ...
+#' @note %% ~~further notes~~
+#' @author %% ~~who you are~~
+#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
+#' @references %% ~put references to the literature/web site here ~
+#' @keywords ~kwd1 ~kwd2
+#' @examples
+#' 
+#' ##---- Should be DIRECTLY executable !! ----
+#' ##-- ==>  Define data, use random,
+#' ##--	or do  help(data=index)  for the standard data sets.
+#' 
+#' ## The function is currently defined as
+#' function (path = NULL, model = NULL, ...) 
+#' {
+#'     if (is.null(path)) 
+#'         path = getwd()
+#'     if (is.null(model)) {
+#'         model = "default"
+#'         warning("Using default demo 'PoissonMixedModel'")
+#'     }
+#'     output = switch(model, Poisson = .generatePoissonMixedModel(path = path, 
+#'         ...), .generatePoissonMixedModel(path = path, ...))
+#'     return(output)
+#'   }
+#' 
 calibrarDemo = function(path=NULL, model=NULL,  ...) {
   
   if(is.null(path)) path = getwd()
--- R/calibraR-test_functions.R
+++ R/calibraR-test_functions.R
@@ -1,4 +1,3 @@
-
 # Test functions ----------------------------------------------------------
 
 # method for plotting (2D and 3D)
@@ -9,6 +8,24 @@ summary.calibrar.function = function(object, ...) {
   return(invisible())
 }
 
+#' Sphere function with random noise 
+#' 
+#' This function calculates the Euclidian distance from a point to the origin
+#' after a random displacement of it position. 
+#' 
+#' @param x The coordinates of the point
+#' @param sd The standard deviation of the noise
+#' to be added to the position of \code{x}, a normal distribution with mean
+#' zero is used.
+#' @param aggregate If \code{aggregate} is \code{TRUE} the distance is returned, 
+#' otherwise the size of the projection of the distance among each axis.
+#' @return The distance from the point \code{x} to the origin after a random
+#' displacement.
+#' @author Ricardo Oliveros--Ramos
+#' @keywords stochastic random
+#' @examples
+#' SphereN(rep(0, 10))
+#' @export
 SphereN = function(x, sd=0.1, aggregate=TRUE) {
   # f(0,...,0) = 0
   # x_i \in ]-Inf, Inf[
