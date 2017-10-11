510c276e1a9018c8d3bf1efce10cea7f97b54b2c
barriern <nicolas.barrier@ird.fr>
2017-10-11 16:20:59 +0200

    Adding a copyMaster function

--- R/calibraR-AHR_ES.R
+++ R/calibraR-AHR_ES.R
@@ -85,7 +85,7 @@
 
 # Calculate Fitness -------------------------------------------------------
 
-
+  
 .calculateFitness = function(opt, fn) {
   
   fn = match.fun(fn)
@@ -107,6 +107,9 @@
       Fitness = c(i+1, Fitness)
       Fitness
       
+      # barrier.n: reset the proper working directory
+      setwd(path.tmp)
+      
     }
     
     FITNESS = FITNESS[sort(FITNESS[,1], index.return=TRUE)$ix,][,-1, drop=FALSE]
@@ -121,6 +124,9 @@
 
       Fitness = fn(pop[,i+1])
       FITNESS	=	rbind(FITNESS, Fitness)
+      
+      # barrier.n: reset the proper working directory
+      setwd(path.tmp)
       
     }
     
@@ -291,5 +297,3 @@
   return(opt)
   
 }
-
-
