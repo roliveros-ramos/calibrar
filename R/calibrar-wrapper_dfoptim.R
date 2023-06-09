ed42d3dba5f3278c57c5b7ddaba92293477383f5
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2023-06-09 22:18:27 +0200

    new wrapper for dfoptim

--- R/calibrar-wrapper_dfoptim.R
+++ R/calibrar-wrapper_dfoptim.R
@@ -3,24 +3,29 @@
 
 .dfoptim = function(par, fn, gr, lower, upper, control, hessian, method) {
   
-  pathTmp = getwd()               # get the current path
-  on.exit(setwd(pathTmp))         # back to the original path after execution
+  if(!is.null(control$trace)) {
+    control$trace = ifelse(control$trace>1, TRUE, FALSE)
+    control$info = control$trace
+  } 
   
-  parNames = names(par)
-  
-  control$trace = TRUE # 
-  control$info = control$trace
-  
-  ctl = switch(method,
+  con = switch(method,
                mads = list(tol = 1e-06, 
                            maxfeval = 10000, 
                            maximize = FALSE, 
                            pollStyle = "lite", deltaInit = 0.01, expand = 4, 
-                           lineSearch = 20, seed = 880820, trace = FALSE),
+                           lineSearch = 20, seed = 1138, trace = FALSE),
+               hjk  = list(tol = 1e-06, 
+                           maxfeval = Inf, 
+                           maximize = FALSE, 
+                           target = Inf, info = FALSE),
                hjkb = list(tol = 1e-06, 
                            maxfeval = Inf, 
                            maximize = FALSE, 
                            target = Inf, info = FALSE),
+               nmk  = list(tol = 1e-06, 
+                           maxfeval = min(5000, max(1500, 20 * length(par)^2)), 
+                           maximize = FALSE,
+                           regsimp = TRUE, restarts.max = 3, trace = FALSE),
                nmkb = list(tol = 1e-06, 
                            maxfeval = min(5000, max(1500, 20 * length(par)^2)), 
                            maximize = FALSE,
@@ -28,7 +33,7 @@
   )
   
   
-  control = NULL # check!
+  control = check_control(control=control, default=con)
   
   out = suppressWarnings(switch(method,
                                 mads = mads(par=par, fn=fn, lower=lower, upper=upper, scale=1, control=control),
@@ -41,31 +46,7 @@
   
   output = list(par=par, value=value, counts=counts, convergence=0)
   
-  # tol
-  # maxfeval
-  # maximize
-  
-  # mads control  
-  # trace
-  # pollStyle
-  # deltaInit
-  # expand
-  # lineSearch
-  # seed
-  
-  # nmkb control
-  # regsimp
-  # restarts.max
-  # trace
-  
-  # hjkb control
-  # target
-  # info
-  
   return(output)
   
 }
 
-# mads(par, fn, lower=-Inf, upper=Inf, scale=1, control = list(), ...)
-# hjkb(par, fn, lower = -Inf, upper = Inf, control = list(), ...)
-# nmkb(par, fn, lower=-Inf, upper=Inf, control = list(), ...)
