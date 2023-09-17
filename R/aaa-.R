457e99b0dec33dd369575221d648f339a6e109af
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2023-09-17 14:23:07 -0500

    adding missing imports.

--- R/aaa-.R
+++ R/aaa-.R
@@ -1,8 +1,11 @@
 #' @importFrom foreach foreach %dopar%
 NULL
-#' @importFrom stats optim pnorm rexp rnorm rpois runif weighted.mean
+#' @importFrom graphics abline points
 NULL
-#' @importFrom utils as.relistable read.csv relist write.csv
+#' @importFrom stats optim pnorm rexp rnorm rpois runif weighted.mean coef 
+#' complete.cases filter lm na.omit predict sigma splinefun
+NULL
+#' @importFrom utils as.relistable read.csv relist write.csv head tail type.convert
 NULL
 #' @importFrom stringr str_split str_trim str_sub str_locate
 NULL
