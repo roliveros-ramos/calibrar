7af91efb0df90c81e453f8d4e1925df632091533
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2024-02-10 19:22:43 +0100

    removing unused all code (testing Rvmmin).

--- R/calibrar-wrapper_optimr.R
+++ R/calibrar-wrapper_optimr.R
@@ -21,24 +21,24 @@
   
 }
 
-.Rcgmin_old = function(par, fn, gr, lower, upper, control, hessian, method) {
-  
-  npar = length(par)
-  # defaults for Rcgmin (taken from Rcgmin::Rcgmin)
-  con = list(maxit = 500, maximize = FALSE, trace = 0, eps = 1e-07, 
-             dowarn = TRUE, tol = 0, checkgrad=FALSE)
-  
-  control = check_control(control=control, default=con)
-  
-  output = suppressWarnings(Rcgmin::Rcgmin(par=par, fn=fn, gr=gr, lower=lower, 
-                                           upper=upper, control=control))
-  names(output$counts) = c('function', 'gradient')
-  # let's ensure there's always the hessian component listed.
-  if(is.null(output$hessian)) output$hessian = NULL
-  
-  return(output)
-  
-}
+# .Rcgmin_old = function(par, fn, gr, lower, upper, control, hessian, method) {
+#   
+#   npar = length(par)
+#   # defaults for Rcgmin (taken from Rcgmin::Rcgmin)
+#   con = list(maxit = 500, maximize = FALSE, trace = 0, eps = 1e-07, 
+#              dowarn = TRUE, tol = 0, checkgrad=FALSE)
+#   
+#   control = check_control(control=control, default=con)
+#   
+#   output = suppressWarnings(Rcgmin::Rcgmin(par=par, fn=fn, gr=gr, lower=lower, 
+#                                            upper=upper, control=control))
+#   names(output$counts) = c('function', 'gradient')
+#   # let's ensure there's always the hessian component listed.
+#   if(is.null(output$hessian)) output$hessian = NULL
+#   
+#   return(output)
+#   
+# }
 
 
 # Rvmmin ------------------------------------------------------------------
@@ -50,7 +50,7 @@
   con = list(maxit = 500 + 2L*npar, maxfeval = 3000 + 10L*npar, maximize = FALSE, 
              trace = 0, eps = 1e-07, dowarn = TRUE, acctol = 1e-04, 
              stepredn = 0.2, reltest = 100, stopbadupdate = TRUE, 
-             checkgrad=FALSE)
+             checkgrad=FALSE, restart.file=NULL, REPORT=1L)
   
   control = check_control(control=control, default=con)
   
@@ -64,47 +64,47 @@
   
 }
 
-.Rvmmin_ror = function(par, fn, gr, lower, upper, control, hessian, method) {
-  
-  npar = length(par)
-  # defaults for Rvmmin (taken from Rvmmin::Rvmmin)
-  con = list(maxit = 500 + 2L*npar, maxfeval = 3000 + 10L*npar, maximize = FALSE, 
-             trace = 0, eps = 1e-07, dowarn = TRUE, acctol = 1e-04, 
-             stepredn = 0.2, reltest = 100, stopbadupdate = TRUE, 
-             checkgrad=FALSE)
-  
-  control = check_control(control=control, default=con)
-  
-  output = suppressWarnings(Rvmmin_ror(par=par, fn=fn, gr=gr, lower=lower, 
-                                    upper=upper, control=control))
-  
-  # let's ensure there's always the hessian component listed.
-  if(is.null(output$hessian)) output$hessian = NULL
-  
-  return(output)
-  
-}
+# .Rvmmin_ror = function(par, fn, gr, lower, upper, control, hessian, method) {
+#   
+#   npar = length(par)
+#   # defaults for Rvmmin (taken from Rvmmin::Rvmmin)
+#   con = list(maxit = 500 + 2L*npar, maxfeval = 3000 + 10L*npar, maximize = FALSE, 
+#              trace = 0, eps = 1e-07, dowarn = TRUE, acctol = 1e-04, 
+#              stepredn = 0.2, reltest = 100, stopbadupdate = TRUE, 
+#              checkgrad=FALSE)
+#   
+#   control = check_control(control=control, default=con)
+#   
+#   output = suppressWarnings(Rvmmin_ror(par=par, fn=fn, gr=gr, lower=lower, 
+#                                     upper=upper, control=control))
+#   
+#   # let's ensure there's always the hessian component listed.
+#   if(is.null(output$hessian)) output$hessian = NULL
+#   
+#   return(output)
+#   
+# }
 
-.Rvmmin_old = function(par, fn, gr, lower, upper, control, hessian, method) {
-  
-  npar = length(par)
-  # defaults for Rvmmin (taken from Rvmmin::Rvmmin)
-  con = list(maxit = 500 + 2L*npar, maxfeval = 3000 + 10L*npar, maximize = FALSE, 
-             trace = 0, eps = 1e-07, dowarn = TRUE, acctol = 1e-04, 
-             stepredn = 0.2, reltest = 100, stopbadupdate = TRUE, 
-             checkgrad=FALSE)
-  
-  control = check_control(control=control, default=con)
-  
-  output = suppressWarnings(Rvmmin::Rvmmin(par=par, fn=fn, gr=gr, lower=lower, 
-                                       upper=upper, control=control))
-  
-  # let's ensure there's always the hessian component listed.
-  if(is.null(output$hessian)) output$hessian = NULL
-  
-  return(output)
-  
-}
+# .Rvmmin_old = function(par, fn, gr, lower, upper, control, hessian, method) {
+#   
+#   npar = length(par)
+#   # defaults for Rvmmin (taken from Rvmmin::Rvmmin)
+#   con = list(maxit = 500 + 2L*npar, maxfeval = 3000 + 10L*npar, maximize = FALSE, 
+#              trace = 0, eps = 1e-07, dowarn = TRUE, acctol = 1e-04, 
+#              stepredn = 0.2, reltest = 100, stopbadupdate = TRUE, 
+#              checkgrad=FALSE)
+#   
+#   control = check_control(control=control, default=con)
+#   
+#   output = suppressWarnings(Rvmmin::Rvmmin(par=par, fn=fn, gr=gr, lower=lower, 
+#                                        upper=upper, control=control))
+#   
+#   # let's ensure there's always the hessian component listed.
+#   if(is.null(output$hessian)) output$hessian = NULL
+#   
+#   return(output)
+#   
+# }
 
 # optimx::hjn -------------------------------------------------------------
 
